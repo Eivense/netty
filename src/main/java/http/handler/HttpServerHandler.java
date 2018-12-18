bc613157023087125ca28dac7279130fca3d564e
Eivense <hexplosives96@gmail.com>
2018-12-19 00:35:58 +0800

    add log4j2 commons-codec

--- src/main/java/http/handler/HttpServerHandler.java
+++ src/main/java/http/handler/HttpServerHandler.java
@@ -3,16 +3,37 @@ package http.handler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.handler.codec.http.*;
+import lombok.extern.log4j.Log4j2;
+import org.apache.commons.codec.CharEncoding;
+import org.apache.commons.codec.Charsets;
 
+
+@Log4j2
 public class HttpServerHandler extends SimpleChannelInboundHandler<HttpObject> {
+
 
     private HttpHeaders headers;
     private HttpRequest request;
     private FullHttpRequest fullHttpRequest;
 
+    private static final String FAVICON_ICO = "/favicon.ico";
+
 
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, HttpObject msg) throws Exception {
+        if(msg instanceof HttpRequest){
+            request= (HttpRequest) msg;
+            headers=request.headers();
+            String uri=request.uri();
+            log.info("http uri:"+uri);
 
+            if(uri.equals(FAVICON_ICO)){
+                return;
+            }
+            HttpMethod method=request.method();
+            if(method.equals(HttpMethod.GET)){
+                QueryStringDecoder queryDecoder=new QueryStringDecoder(uri, Charsets.toCharset(CharEncoding.UTF_8));
+            }
+        }
     }
 }
